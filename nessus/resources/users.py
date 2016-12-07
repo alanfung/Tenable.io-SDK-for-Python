1b55ceb6e4880f32cf6e883f26de0dc27fd172f0
Kevin Shi <kevin@codeparticle.com>
2016-12-07 14:06:37 -0800

    Integrated user create, edit, & delete API
    * Added tests for the respective calls.
    
    Issue: TAPI-220 TAPI-223 TAPI-221

--- nessus/resources/users.py
+++ nessus/resources/users.py
@@ -1,6 +1,6 @@
-import json
+from json import loads
 
-from nessus.resources.base import BaseResource
+from nessus.resources.base import BaseResource, BaseRequest
 from nessus.resources.models import User, UserList
 
 
@@ -15,5 +15,49 @@ class UsersResource(BaseResource):
         return UserList.from_json(response.text)
 
     def impersonate(self, user_id):
-        response = self._client.post('users/%(user_id)/impersonate', path_params={'user_id': user_id})
-        return json.loads(response.text)
+        response = self._client.post('users/%(user_id)s/impersonate', path_params={'user_id': user_id})
+        return loads(response.text)
+
+    def create(self, user_create):
+        response = self._client.post('users', user_create)
+        return loads(response.text).get('id')
+
+    def edit(self, user_id, user_edit):
+        response = self._client.put('users/%(user_id)s', user_edit, {'user_id': user_id})
+        return User.from_json(response.text)
+
+    def delete(self, user_id):
+        self._client.delete('users/%(user_id)s', {'user_id': user_id})
+        return True
+
+
+class UserCreateRequest(BaseRequest):
+
+    def __init__(
+            self,
+            username=None,
+            password=None,
+            permissions=None,
+            name=None,
+            email=None,
+            type=None
+    ):
+        self.username = username
+        self.password = password
+        self.permissions = permissions
+        self.name = name
+        self.email = email
+        self.type = type
+
+
+class UserEditRequest(BaseRequest):
+
+    def __init__(
+            self,
+            permissions=None,
+            name=None,
+            email=None
+    ):
+        self.permissions = permissions
+        self.name = name
+        self.email = email
