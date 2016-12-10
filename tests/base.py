2054131c4da06b769f8d6f8052ca3bd22109e59b
Alan Fung <alan@alanfung.com>
2016-12-09 17:22:59 -0800

    Print test duration

--- tests/base.py
+++ tests/base.py
@@ -1,7 +1,14 @@
+from time import time
+
+
 class BaseTest(object):
 
     def setup_method(self, method):
-        print "\n%s:%s" % (type(self).__name__, method.__name__)
+        if not hasattr(self, '_timer'):
+            self._timer = {}
+        print("\n%s:%s is running." % (type(self).__name__, method.__name__))
+        self._timer[method.__name__] = time()
 
     def teardown_method(self, method):
-        pass
+        duration = time() - self._timer[method.__name__]
+        print("\n%s:%s took (%s seconds)." % (type(self).__name__, method.__name__, duration))
