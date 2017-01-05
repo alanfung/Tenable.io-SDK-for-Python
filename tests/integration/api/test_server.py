5ab0755933d79b820fd6164b88abc6d51458d738
Kevin Shi <kevin@codeparticle.com>
2017-01-04 18:19:12 -0800

    Refactor API instance in NessusClient
    Issue: TAPI-254

--- tests/integration/api/test_server.py
+++ tests/integration/api/test_server.py
@@ -5,11 +5,11 @@ from tests.base import BaseTest
 class TestServerApi(BaseTest):
 
     def test_server_properties(self, client):
-        server_properties = client.server.properties()
+        server_properties = client.server_api.properties()
 
         assert isinstance(server_properties, ServerProperties), u'The `property` method return type.'
 
     def test_server_status(self, client):
-        server_status = client.server.status()
+        server_status = client.server_api.status()
 
         assert isinstance(server_status, ServerStatus), u'The `status` method return type.'
