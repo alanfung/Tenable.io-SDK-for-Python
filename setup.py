21a4dde2a4b5a211b7bb13850539e0b65a7594bc
Kevin Shi <kevin@codeparticle.com>
2017-01-16 18:51:26 -0800

    Initial setup for wheel packaging.
    Issue: TAPI-261

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md')) as file:
+    long_description = file.read()
+
+setup(
+    name='nessus',
+    version='0.0',
+    description='Tenable Nessus API SDK',
+    long_description=long_description,
+    author='Code Particle',
+    packages=find_packages(exclude=['doc', 'tests*']),
+    install_requires=[
+        "mock==2.0.0",
+        "pytest ==3.0.4",
+        "requests==2.12.1",
+        "six==1.10.0",
+        "Sphinx==1.5.1",
+    ],
+)
